1ba078ec390c10947eb2b3461d392956cfc08a36
Isadora Garcia <isadoragarciaalmeidasilva7450@gmail.com>
2025-06-24 14:37:09 -0300

    FEAT: componente de filtro

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -2,20 +2,28 @@ import { Image, Text, View } from "react-native";
 import { styles } from "./styles";
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
+import { Filter } from "@/components/Filter";
+import { FilterStatus } from "@/types/FilterStatus";
 
 export function Home() {
   return(
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo } />
-      
-      <Input 
-        placeholder="O que você precisa comprar?"
-      />
 
-      <Button 
-        title="Adicionar" 
-        onPress={() => console.log("oi")} 
-      />
+      <View style={styles.form}>
+        <Input 
+          placeholder="O que você precisa comprar?"
+        />
+        <Button 
+          title="Adicionar" 
+          onPress={() => console.log("Adicionado")} 
+        />
+      </View>
+
+      <View style={styles.content}>
+        <Filter status={FilterStatus.PENDING} isActive={false} />
+        <Filter status={FilterStatus.DONE} isActive />
+      </View>
     </View>
   );
 }
--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -5,11 +5,26 @@ export const styles = StyleSheet.create({
         flex: 1,
         alignItems: "center",
         paddingTop: 62,
-        padding: 24,
         backgroundColor: "#D0D2D8",
     },
     logo: {
         height: 34,
         width: 134,
+    },
+    form: {
+        width: "100%",
+        paddingHorizontal: 16,
+        gap: 7,
+        marginTop: 42,
+    },
+    content: {
+        flex: 1,
+        width: "100%",
+        backgroundColor: "#FFF",
+        borderTopLeftRadius: 24,
+        borderTopRightRadius: 24,
+        padding: 24,
+        marginTop: 24,
+
     }
 })
